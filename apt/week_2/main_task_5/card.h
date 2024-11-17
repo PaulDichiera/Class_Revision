943894020329a71d7f68e02a5aa4709476ccbcf0
PaulDichiera <paul.dichiera@gmail.com>
2024-11-17 18:40:45 +1100

    completed task 5 and updated the notes file with learnings from the last few tasks

---
+++ apt/week_2/main_task_5/card.h
@@ -0,0 +1,29 @@
+#ifndef CARD_H
+#define CARD_H
+#include <iostream>
+
+class Card{ // class and class name
+
+    // methods to create 
+    // - Return the cards number
+    // - Return the cards colour represented as a number
+    // - Return the cards colour as a string 
+    // - Return the cards rule as a string
+    private:
+        int number;
+        int colour;
+        std::string colourString;
+        std::string rule;
+
+
+    public: // variables or methods from the class that are public
+
+        Card(int number, int colour);
+
+        int returnNumber();
+        int returnColour();
+        std::string returnColourStr();
+        std::string returnRule();
+};
+
+#endif "CARD_H"
