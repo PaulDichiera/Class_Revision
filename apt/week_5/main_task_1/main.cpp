31479f78a46753ae9bed824637d0d5992b1fafd9
PaulDichiera <paul.dichiera@gmail.com>
2024-12-28 19:47:51 +1100

    starting week 5 creating a script that can open a file a nd read the contents

--- apt/week_5/main_task_1/main.cpp
+++ apt/week_5/main_task_1/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
-int main(void){
+int main(int argc, char *argv[]){
 
-    
+    std::cout << "Number of arguments: " << argc << std::endl;
+
+    std::string processType = argv[1];
+    std::string fileName = argv[2];
+    std::string line;
+
+    std::cout << "File name: " << fileName << std::endl;
+
+    std::ifstream file(fileName);
+
+    if(file.is_open()){
+        std::cout << "File Open" << std::endl;
+            while(std::getline(file, line)){
+            std::cout << line << std::endl;
+            
+        }
+    }
+
+    while(std::getline(file, line)){
+        std::cout << "here" << std::endl;
+        std::cout << line << std::endl;
+        
+    }
+
+    file.close();
+
+    if(!file.is_open()){
+        std::cout << "File Closed" << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
