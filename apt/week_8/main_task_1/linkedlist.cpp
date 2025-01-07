f1018b89e0fc98a1f47f04a55c3f199d201eba42
PaulDichiera <paul.dichiera@gmail.com>
2025-01-07 18:25:12 +1100

    created another linked list, added addAt and deleteAt, they need a little work but are functioing correclty

---
+++ apt/week_8/main_task_1/linkedlist.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+
+#include "linkedlist.h"
+
+LinkedList::LinkedList(){
+    this->head = nullptr;
+}
+
+LinkedList::~LinkedList(){
+    //Todo
+    //clear();
+}
+
+int LinkedList::size(){
+    int count = 0;
+
+    if(head == nullptr){
+        std::cout << "** List is empty **" << std::endl;
+    }else{
+        Node* temp = head;
+        while(temp != nullptr){
+            count++;
+            temp = temp->next;
+        }
+    }
+    return count;
+}
+
+void LinkedList::clear(){
+    if(head == nullptr){
+        std::cout << "** List is empty, delete action aborted **" << std::endl;
+    }else{
+        Node* remove = head;
+        Node* temp = head;
+        while(temp != nullptr){
+            remove = temp;
+            temp = temp->next;
+            std::cout << "** deleting node **" << std::endl;
+            delete remove;
+        }
+        head = nullptr;
+    }
+}
+
+int LinkedList::get(int index){
+    int result = -1;
+    if(head == nullptr){
+        std::cout << "** List is empty, get action aborted **" << std::endl;
+    }else if(index > size()){
+        std::cout << "** Selection above possible range, get action aborted **" << std::endl;
+    }else if(index < 0){
+        std::cout << "** Selection below possible range, get action aborted **" << std::endl;
+    }else{
+        int count = 0;
+        Node* temp = head;
+        while(temp != nullptr){
+            if(count == index){
+                result = temp->data;
+            }
+            count++;
+            temp = temp->next;
+        }
+    }
+    return result;
+}
+
+void LinkedList::addFront(int data){
+    Node* newNode = new Node(data);
+    if(head == nullptr){
+        head = newNode;
+    }else{
+        newNode->next = head;
+        head = newNode;
+    }
+    std::cout << "** Node: " << newNode->data << " added to front **" << std::endl;
+}
+
+void LinkedList::addBack(int data){
+    if(head == nullptr){
+        addFront(data);
+    }else{
+        Node* temp = head;
+        Node* newNode = new Node(data);
+        while(temp->next != nullptr){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        std::cout << "** Node: " << newNode->data << " added to front **" << std::endl;
+    }
+}
+
+void LinkedList::deleteFront(){
+    if(head == nullptr){
+        std::cout << "** List is empty, delete front action aborted **" << std::endl;
+    }else{
+        Node* remove = head;
+        head = head->next;
+        delete remove;
+        std::cout << "** Front node deleted **" << std::endl;
+    }
+}
+
+void LinkedList::deleteBack(){
+    if(head == nullptr){
+        std::cout << "** List is empty, delete back action aborted **" << std::endl;
+    }else{
+        Node* remove = head;
+        Node* temp = head;
+        while(temp->next->next != nullptr){
+            temp = temp->next;
+        }
+        remove = temp->next;
+        delete remove;
+        temp->next = nullptr;
+        std::cout << "** Front node deleted **" << std::endl;
+    }
+}
+
+void LinkedList::addAt(int index, int data){
+    if(head == nullptr){
+        std::cout << "** List is empty, add at action aborted **" << std::endl;
+    }else if(index > size()){
+        std::cout << "** Selection above possible range, add at action aborted **" << std::endl;
+    }else if(index < 0){
+        std::cout << "** Selection below possible range, add at action aborted **" << std::endl;
+    }else if(index == 0){
+        addFront(data);
+    }else{
+        int count = 0;
+        Node* temp = head;
+        while(temp != nullptr){
+            if(count == index - 1){
+                Node* newNode = new Node(data);
+                newNode->next = temp->next;
+                temp->next = newNode;
+                std::cout << "Node: " << newNode->data << " added at index" << count << std::endl; 
+            }
+            std::cout << "entered" << std::endl;
+            count++;
+            temp = temp->next;
+        }
+    }
+}
+
+void LinkedList::deleteAt(int index){
+    if(head == nullptr){
+        std::cout << "** List is empty, delete at action aborted **" << std::endl;
+    }else{
+        int count = 0;
+        Node* remove = head;
+        Node* temp = head;
+        while(temp->next != nullptr){
+            if(count == index - 1){
+                std::cout << "** Node: " << temp->data << " deleted at index " << count << std::endl;
+                remove = temp->next;
+                temp->next = remove->next;
+                delete remove;
+            }
+            temp = temp->next;
+            count++;
+        }
+    }
+}
---
+++ apt/week_8/main_task_1/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+#include "linkedlist.h"
+
+void printList(LinkedList* list);
+void printSize(LinkedList* list);
+
+int main(void){
+
+    LinkedList* list = new LinkedList();
+
+    list->addFront(88);
+    list->addFront(23);
+    list->addBack(99);
+    list->addBack(105);
+    list->addFront(2);
+
+    printSize(list);
+    printList(list);
+
+    list->deleteFront();
+    list->deleteFront();
+
+    printSize(list);
+    printList(list);
+
+    list->deleteBack();
+    list->deleteBack();
+
+    printSize(list);
+    printList(list);
+
+    list->addBack(200);
+    list->addFront(1);
+
+    list->addAt(1, 55);
+
+    printSize(list);
+    printList(list);
+
+    list->deleteAt(1);
+
+    printSize(list);
+    printList(list);
+
+    return EXIT_SUCCESS;
+}
+
+void printList(LinkedList* list){
+    std::cout << "** List Print Out **" << std::endl;
+    std::cout << std::endl;
+    for(int i = 0; i < list->size(); i++){
+        std::cout << "List data: " << list->get(i) << " at index " << i << std::endl;
+    }
+}
+
+void printSize(LinkedList* list){
+    std::cout << std::endl;
+    std::cout << "** List size currently: " << list->size() << " **" <<  std::endl;
+    std::cout << std::endl;
+}
